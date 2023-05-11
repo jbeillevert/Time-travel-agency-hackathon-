d313991824d00555c41aa96d20e0318af0854739
ROMARIC LAMARE <108812597+RomaricLamare@users.noreply.github.com>
2023-05-11 16:07:28 +0200

    t1 t5 et t8 créer HomePage, créer SimpleCard, faire le routeur

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Router from "./components/router/Router"
+import NavBar from "./NavBar"
+
 function App() {
   return (
     <div className="app">
-
+      <NavBar />
+      <Router />
     </div>
   )
 }
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+
+function HomePage() {
+
+const [data, setData] = useState([])
+const [search, setSearch] = useState("")
+
+useEffect(() => {
+    fetch("https://umnptqfditgysgbpzoyx.supabase.co/")
+    .then((response) => response.json())
+    .then((res) => setData(res))
+    .catch((err) => console.error(err))   
+   }, [])
+
+   const handleSelector = (e) => {
+    setSearch(e.target.value)
+}
+
+    return (
+        <main className="home-page">
+                <select className="home-page_select" value={search} onChange={handleSelector}>
+                    <option className="option" value="">...</option>
+                    {data.map((el) => {
+                        return(
+                            <option key={el.id} value={el.id}>{el.titre}</option>
+                        )
+                    })}
+                </select>
+                <ul className="home-page_ul">
+                {data
+                    .filter((el) => search === "" || el.titre === search)
+                    .map((el) => {
+                        return(
+                        <SingleCard key={el.id} image={el.image} titre={el.titre} tarif={el.tarif} id={i.id} />
+                        ) 
+                    })}
+                </ul>
+            </main>
+    );
+}
+
+export default HomePage;

500282b50f3639c9a7f18e93b87ad163098c03d1
thibault <thibaultbenoit0@gmail.com>
2023-05-11 19:07:54 +0200

    css cards en cours

--- src/components/pages/CardDetails.jsx
+++ src/components/pages/CardDetails.jsx
@@ -8,12 +8,12 @@ function CardDetails() {
       <img src={ImageDetails} alt="photo_monument_chine" />
     </div>
     <div className="DescriptionCardDetails">
-      <h1>Dynastie Zhou de l'Est</h1>
-      <p>La dynastie des Zhou orientaux chinois traditionnel : 東周 ; chinois simplifié : 东周 ; pinyin : dōng zhōu, qui commence en -770 et se termine en -2561,2, ayant duré pendant la période des Printemps et des Automnes et pendant les Royaumes combattants. C'est une période réduite de la dynastie Zhou (-1045 — -256) qui fait suite à la période dite de la dynastie Zhou occidentale. Sa capitale était Luoyi (chinois : 洛邑 ; pinyin : luòyì ; litt. « ville ou district de Luo »), aujourd'hui, Luoyang. Son fondateur est Zhou Pingwang et la chute de la dynastie a lieu sous le règne de Zhou Nanwang.</p> 
+      <h1 className="title-details">Dynastie Zhou de l'Est</h1>
+      <p className="description-details">La dynastie des Zhou orientaux chinois traditionnel : 東周 ; chinois simplifié : 东周 ; pinyin : dōng zhōu, qui commence en -770 et se termine en -2561,2, ayant duré pendant la période des Printemps et des Automnes et pendant les Royaumes combattants. C'est une période réduite de la dynastie Zhou (-1045 — -256) qui fait suite à la période dite de la dynastie Zhou occidentale. Sa capitale était Luoyi (chinois : 洛邑 ; pinyin : luòyì ; litt. « ville ou district de Luo »), aujourd'hui, Luoyang. Son fondateur est Zhou Pingwang et la chute de la dynastie a lieu sous le règne de Zhou Nanwang.</p> 
     </div>
     <div className="ButtonReservation">
       <h3 className="PriceCard">Destination : Chine , année -770</h3>
-      <button>Reserver</button>
+      <button className="button-details">Reserver</button>
       <h3 className="PriceCard">80000 euros le transfert</h3>
 
     </div>

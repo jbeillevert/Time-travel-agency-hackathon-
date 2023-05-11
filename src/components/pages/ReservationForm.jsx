6d7b951d25f2936e6aff428a7c827bdaa31c6bc2
Jean-Emmanuel Beillevert <beillevertj.contact@gmail.com>
2023-05-11 23:51:31 +0200

    compteur de voyageurs terminé

--- src/components/pages/ReservationForm.jsx
+++ src/components/pages/ReservationForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlineCalendar } from "react-icons/hi";
 import { AiOutlineMinusCircle } from "react-icons/ai";
 import { AiOutlinePlusCircle } from "react-icons/ai";
@@ -7,7 +7,76 @@ import { AiOutlinePlusCircle } from "react-icons/ai";
 
 
 
+
 const ReservationForm = () => {
+    const [initialPrice, setInitialPrice] = useState(72500)
+    const [totalPrice, setTotalPrice] = useState(72500)
+    const [nbAdultes, setNbAdultes] = useState(1)
+    const [nbEnfants, setNbEnfants] = useState(0)
+    const [nbBebes, setNbBebes] = useState(0)
+    const [nbAnimaux, setNbAnimaux] = useState(0)
+    const [nbVoyageursTotal, setNbVoyageursTotal] = useState(1)
+
+    const [imgCommand, setImgCommand] = useState('')
+    const [titleCommand, setTitleCommand] = useState('')
+    const [textCommand, setTextCommand] = useState('')
+
+
+    function handleClickAddAdult() {
+        setNbAdultes(nbAdultes + 1)
+        setNbVoyageursTotal(nbVoyageursTotal + 1)
+    }
+
+    function handleClickRemoveAdult() {
+        if (nbAdultes >= 2) {
+            setNbAdultes(nbAdultes - 1)
+            setNbVoyageursTotal(nbVoyageursTotal - 1)
+        }
+    }
+
+    function handleClickAddEnfant() {
+        setNbEnfants(nbEnfants + 1)
+        setNbVoyageursTotal(nbVoyageursTotal + 1)
+    }
+
+    function handleClickRemoveEnfant() {
+        if (nbEnfants >= 1) {
+            setNbEnfants(nbEnfants - 1)
+            setNbVoyageursTotal(nbVoyageursTotal - 1)
+        }
+    }
+
+    function handleClickAddBaby() {
+        setNbBebes(nbBebes + 1)
+        setNbVoyageursTotal(nbVoyageursTotal + 1)
+    }
+
+    function handleClickRemoveBaby() {
+        if (nbBebes >= 1) {
+            setNbBebes(nbBebes - 1)
+            setNbVoyageursTotal(nbVoyageursTotal - 1)
+        }
+    }
+
+    function handleClickAddAnimal() {
+        setNbAnimaux(nbAnimaux + 1)
+        setNbVoyageursTotal(nbVoyageursTotal + 1)
+    }
+
+    function handleClickRemoveAnimal() {
+        if (nbAnimaux >= 1) {
+            setNbAnimaux(nbAnimaux - 1)
+            setNbVoyageursTotal(nbVoyageursTotal - 1)
+        }
+    }
+
+
+    useEffect(() => {
+        setTotalPrice(nbVoyageursTotal * initialPrice)
+
+    }, [nbVoyageursTotal])
+
+
     return (
         <div className='reservation-form'>
             <form className='form-rf'>
@@ -22,12 +91,12 @@ const ReservationForm = () => {
                                     <input type="text" className='input-rf' id="nom" />
                                     <label htmlFor="email" className='label-rf'>Email</label>
                                     <input type="email" className='input-rf' id='email' />
-                                    <label htmlFor="phone" className='label-rf'>Mobile</label>
+                                    <label htmlFor="phone" className='label-rf'>Téléphone</label>
                                     <input type="tel" className='input-rf' id='phone' pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" />
                                 </div>
                         </div>
                         <div className="block-container-rf">
-                            <h2 className='h2-rf'>Dates départ</h2>
+                            <h2 className='h2-rf'>Choisir un départ</h2>
                             <div className="dates-container">
                                 <div className="dates-proposal-rf">
                                     <label className="custom-radio">
@@ -48,7 +117,7 @@ const ReservationForm = () => {
                     </div>
                     <div className='right-part-reservation-form'>
                         <div className="block-container-rf">
-                            <div className="h2-rf">Réservation</div>
+                            <div className="h2-rf">Votre commande</div>
                             <div className="card-command">
                                 <img src="https://images.voyageschine.com/allpicture/2017/10/8e4b35512a04424497f403f4.jpg" className='img-command' alt="" />
                                 <div className="container-text-command">
@@ -64,9 +133,9 @@ const ReservationForm = () => {
                                         <p className='text-voyageur'>13 ans et plus</p>
                                     </div>
                                     <div className="counter-voyageur">
-                                        <AiOutlineMinusCircle className='icon-minus' />
-                                        <p className='counter-text'>1</p>
-                                        <AiOutlinePlusCircle className='icon-plus' />
+                                        <AiOutlineMinusCircle className='icon-minus' onClick={handleClickRemoveAdult} />
+                                        <p className='counter-text'>{nbAdultes}</p>
+                                        <AiOutlinePlusCircle className='icon-plus' onClick={handleClickAddAdult} />
                                     </div>
                                 </div>
                                 <div className="card-voyageur">
@@ -75,9 +144,9 @@ const ReservationForm = () => {
                                         <p className='text-voyageur'>De 2 à 12 ans</p>
                                     </div>
                                     <div className="counter-voyageur">
-                                        <AiOutlineMinusCircle className='icon-minus' />
-                                        <p className='counter-text'>0</p>
-                                        <AiOutlinePlusCircle className='icon-plus' />
+                                        <AiOutlineMinusCircle className='icon-minus' onClick={handleClickRemoveEnfant} />
+                                        <p className='counter-text'>{nbEnfants}</p>
+                                        <AiOutlinePlusCircle className='icon-plus' onClick={handleClickAddEnfant} />
                                     </div>
                                 </div>
                                 <div className="card-voyageur">
@@ -86,9 +155,9 @@ const ReservationForm = () => {
                                         <p className='text-voyageur'>-de 2 ans</p>
                                     </div>
                                     <div className="counter-voyageur">
-                                        <AiOutlineMinusCircle className='icon-minus' />
-                                        <p className='counter-text'>0</p>
-                                        <AiOutlinePlusCircle className='icon-plus' />
+                                        <AiOutlineMinusCircle className='icon-minus' onClick={handleClickRemoveBaby} />
+                                        <p className='counter-text'>{nbBebes}</p>
+                                        <AiOutlinePlusCircle className='icon-plus' onClick={handleClickAddBaby} />
                                     </div>
                                 </div>
                                 <div className="card-voyageur">
@@ -96,15 +165,25 @@ const ReservationForm = () => {
                                         <h4 className='type-voyageur'>Animaux de compagnie</h4>
                                     </div>
                                     <div className="counter-voyageur">
-                                        <AiOutlineMinusCircle className='icon-minus' />
-                                        <p className='counter-text'>0</p>
-                                        <AiOutlinePlusCircle className='icon-plus' />
+                                        <AiOutlineMinusCircle className='icon-minus' onClick={handleClickRemoveAnimal} />
+                                        <p className='counter-text'>{nbAnimaux}</p>
+                                        <AiOutlinePlusCircle className='icon-plus' onClick={handleClickAddAnimal} />
                                     </div>
                                 </div>
                             </div>
+
+                            <div className="test-container">
+                                <h3 className='text-cgv'>Nombre de voyageurs</h3>
+                                <h3 className='test-h3'>{nbVoyageursTotal}</h3>
+                            </div>
+                            <div className="test-container">
+                                <h3 className='text-cgv'>Prix de base</h3>
+                                <h3 className='test-h3'>{initialPrice} €</h3>
+                            </div>
+
                             <div className="card-price">
                                 <div className="taxes-price">Total T.T.C</div>
-                                <div className="total-price">72 500 €</div>
+                                <div className="total-price">{totalPrice} €</div>
                             </div>
                             <button type="submit" className='button-reservation'>Réserver</button>
                             <p className='text-cgv'>Voir nos <span>conditions générales de vente</span></p>

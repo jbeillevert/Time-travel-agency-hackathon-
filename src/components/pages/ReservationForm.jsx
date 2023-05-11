8c1cf965d5583f63e6f0f66ffd84a0049c889dda
Jean-Emmanuel Beillevert <beillevertj.contact@gmail.com>
2023-05-11 22:38:26 +0200

    CSS terminé

--- src/components/pages/ReservationForm.jsx
+++ src/components/pages/ReservationForm.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { HiOutlineCalendar } from "react-icons/hi";
+import { AiOutlineMinusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
+
 
 
 
@@ -42,7 +46,70 @@ const ReservationForm = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='right-part-reservation-form'></div>
+                    <div className='right-part-reservation-form'>
+                        <div className="block-container-rf">
+                            <div className="h2-rf">Réservation</div>
+                            <div className="card-command">
+                                <img src="https://images.voyageschine.com/allpicture/2017/10/8e4b35512a04424497f403f4.jpg" className='img-command' alt="" />
+                                <div className="container-text-command">
+                                    <h3 className="title-command">Dynastie Zhou de l'est</h3>
+                                    <p className='text-command' >1700 années de transfert</p>
+                                </div>
+                            </div>
+                            <h3 className='block-voyageurs'>Voyageurs</h3>
+                            <div className="container-voyageurs">
+                                <div className="card-voyageur">
+                                    <div className="wrapper-text-voyageur">
+                                        <h4 className='type-voyageur'>Adultes</h4>
+                                        <p className='text-voyageur'>13 ans et plus</p>
+                                    </div>
+                                    <div className="counter-voyageur">
+                                        <AiOutlineMinusCircle className='icon-minus' />
+                                        <p className='counter-text'>1</p>
+                                        <AiOutlinePlusCircle className='icon-plus' />
+                                    </div>
+                                </div>
+                                <div className="card-voyageur">
+                                    <div className="wrapper-text-voyageur">
+                                        <h4 className='type-voyageur'>Enfants</h4>
+                                        <p className='text-voyageur'>De 2 à 12 ans</p>
+                                    </div>
+                                    <div className="counter-voyageur">
+                                        <AiOutlineMinusCircle className='icon-minus' />
+                                        <p className='counter-text'>0</p>
+                                        <AiOutlinePlusCircle className='icon-plus' />
+                                    </div>
+                                </div>
+                                <div className="card-voyageur">
+                                    <div className="wrapper-text-voyageur">
+                                        <h4 className='type-voyageur'>Bébés</h4>
+                                        <p className='text-voyageur'>-de 2 ans</p>
+                                    </div>
+                                    <div className="counter-voyageur">
+                                        <AiOutlineMinusCircle className='icon-minus' />
+                                        <p className='counter-text'>0</p>
+                                        <AiOutlinePlusCircle className='icon-plus' />
+                                    </div>
+                                </div>
+                                <div className="card-voyageur">
+                                    <div className="wrapper-text-voyageur">
+                                        <h4 className='type-voyageur'>Animaux de compagnie</h4>
+                                    </div>
+                                    <div className="counter-voyageur">
+                                        <AiOutlineMinusCircle className='icon-minus' />
+                                        <p className='counter-text'>0</p>
+                                        <AiOutlinePlusCircle className='icon-plus' />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-price">
+                                <div className="taxes-price">Total T.T.C</div>
+                                <div className="total-price">72 500 €</div>
+                            </div>
+                            <button type="submit" className='button-reservation'>Réserver</button>
+                            <p className='text-cgv'>Voir nos <span>conditions générales de vente</span></p>
+                        </div>
+                    </div>
 
                 </div>
 

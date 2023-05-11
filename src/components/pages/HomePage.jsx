2e183e4e8eb98451c0afb786b2f0a6d1435751fd
JeremyHerpe <herpe.jeremy@gmail.com>
2023-05-11 17:28:27 +0200

    supabase linkée

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,43 +1,49 @@
 import React, { useState, useEffect } from 'react';
-
+import { createClient } from "@supabase/supabase-js";
+import SingleCard from '../SingleCard';
 
 function HomePage() {
 
-const [data, setData] = useState([])
-const [search, setSearch] = useState("")
+    // const [data, setData] = useState([])
+    const [search, setSearch] = useState("")
 
-useEffect(() => {
-    fetch("https://umnptqfditgysgbpzoyx.supabase.co/")
-    .then((response) => response.json())
-    .then((res) => setData(res))
-    .catch((err) => console.error(err))   
-   }, [])
+    const [table, setTable] = useState([]);
+    const supabase = createClient("https://umnptqfditgysgbpzoyx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtbnB0cWZkaXRneXNnYnB6b3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4MTA2MjUsImV4cCI6MTk5OTM4NjYyNX0.LExHLdYK2bAdq0ronsaNNl9VDAeMwhTw0SVzB735W5o");
 
-   const handleSelector = (e) => {
-    setSearch(e.target.value)
-}
-console.log("pouet !")
+    useEffect(() => {
+        gettable()
+    }, []);
+
+    async function gettable() {
+        const { data } = await supabase.from("jean").select();
+        setTable(data);
+    }
+
+    const handleSelector = (e) => {
+        setSearch(e.target.value)
+    }
+    console.log("pouet !")
 
     return (
         <main className="home-page">
-                <select className="home-page_select" value={search} onChange={handleSelector}>
-                    <option className="option" value="">...</option>
-                    {data.map((el) => {
-                        return(
-                            <option key={el.id} value={el.id}>{el.titre}</option>
-                        )
-                    })}
-                </select>
-                <ul className="home-page_ul">
-                {data
+            <select className="home-page_select" value={search} onChange={handleSelector}>
+                <option className="option" value="">...</option>
+                {table.map((el) => {
+                    return (
+                        <option key={el.id} value={el.id}>{el.titre}</option>
+                    )
+                })}
+            </select>
+            <ul className="home-page_ul">
+                {table
                     .filter((el) => search === "" || el.titre === search)
                     .map((el) => {
-                        return(
-                        <SingleCard key={el.id} image={el.image} titre={el.titre} tarif={el.tarif} id={i.id} />
-                        ) 
+                        return (
+                            <SingleCard key={el.id} image={el.image} titre={el.titre} tarif={el.tarif} id={el.id} />
+                        )
                     })}
-                </ul>
-            </main>
+            </ul>
+        </main>
     );
 }
 
